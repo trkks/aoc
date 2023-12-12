08376211a68582b8d610d6a503361d3b81cf32ac
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2023-12-12 22:05:27 +0200

    h6 p2

--- src/h6.clj
+++ src/h6.clj
@@ -5,7 +5,7 @@
 (require '[clojure.math :as math])
 
 
-(defn distance [n ms] (int (math/floor (* ms (- n ms)))))
+(defn distance [n ms] (bigint (math/floor (* ms (- n ms)))))
 
 (defn distances [ms] (map #(distance ms %) (range (inc ms))))
 
@@ -17,18 +17,24 @@
       :time (parse ts)
       :distance (parse ds))))
 
+(defn race [[ts ds]]
+  (let [parse (fn [s] (parse-long (str/join (rest (str/split s #"\s+")))))]
+    [[(parse ts)] [(parse ds)]]))
+
+(defn winways [ts ds]
+  (map
+    #(count (winners (first %) (second %)))
+    (map vector (map distances ts) ds)))
+
 (defn p1' [s]
   (let [m (races (str/split-lines s))]
-    (apply *
-      (map
-        #(count (winners (first %) (second %)))
-        (map vector (map distances (:time m)) (:distance m))))))
+    (apply * (winways (:time m) (:distance m)))))
         
-(def p2' identity)
+(defn p2' [s] (apply winways (race (str/split-lines s))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn run [f] (println (f (slurp "resources/h6.dat"))))
 
 (defn p1 [& _] (run p1'))
-(defn p2 [& _] (println "unfinished") (run p2'))
+(defn p2 [& _] (run p2'))
