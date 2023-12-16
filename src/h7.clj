8c35643f7c936ea349ec4a3fdff9420de32e0758
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2023-12-16 14:53:25 +0200

    h7 p2

--- src/h7.clj
+++ src/h7.clj
@@ -5,67 +5,110 @@
 (require '[clojure.math :as math])
 
 
-(def card-values [ nil \2 \3 \4 \5 \6 \7 \8 \9 \T \J \Q \K \A ])
+(def basic-card-values [ nil \2 \3 \4 \5 \6 \7 \8 \9 \T \J \Q \K \A ])
+(def jokered-card-values [ nil \J \2 \3 \4 \5 \6 \7 \8 \9 \T \Q \K \A ])
 
-(defn card-value-based [xs]
+(defn card-value-based [cvs xs]
   (apply +
     (map-indexed
-      #(long (* %2 (math/pow (count card-values) %1)))
+      #(long (* %2 (math/pow (count cvs) %1)))
       (reverse xs))))
 
-(defn card-strength' [s]
-  (card-value-based (map #(.indexOf card-values %) s)))
+(defn card-strength' [cvs s]
+  (card-value-based cvs (map #(.indexOf cvs %) s)))
 
-(defn card-strength [m]
+(defn card-strength [cvs m]
   (let [hand (:hand m)]
-    (card-strength' hand)))
+    (card-strength' cvs hand)))
+
+(defn strongest-frequentistest-value [cvs s]
+  (let [values (set cvs)
+        s' (filter values s)]
+    (reduce-kv
+      #(if (> %3 (:v %1))
+        {:k %2, :v %3}
+        %1)
+      {:k nil, :v 0}
+      (frequencies s'))))
+
+(defn jokered? [cvs] (= (get cvs 1) \J))
+
+(defn jokers [cvs s]
+  (let [j (or (get (frequencies s) \J) 0)]
+    (if (jokered? cvs) j 0)))
+
+(defn hand-type' [cvs s]
+  (let [{k :k, v :v} (strongest-frequentistest-value cvs s)
+        snk (filter #(not= % k) s)
+        j (jokers cvs snk)
+        s' (if (jokered? cvs) (filter #(not= % \J) snk) snk)]
+    (case v
+      5 :fioak
+
+      4 (if (= j 1)
+          :fioak
+          (if (and (jokered? cvs) (= k \J)) :fioak :fooak))
+
+      3 (cond
+          (= j 2)
+            :fioak
+          (= j 1)
+            :fooak
+          :else
+            (if (and (jokered? cvs) (= k \J))
+              (if (= :op (hand-type' cvs s')) :fioak :fooak)
+              (if (= :op (hand-type' cvs s')) :fh :toak)))
+
+      2 (cond
+          (= j 3)
+            :fioak
+          (= j 2)
+            :fooak
+          (= j 1)
+            (if (= :op (hand-type' cvs s')) :fh :toak)
+          :else
+            (if (and (jokered? cvs) (= k \J))
+              (if (= :op (hand-type' cvs s')) :fooak :toak)
+              (if (= :op (hand-type' cvs s')) :tp :op)))
+
+      1 (cond
+          (= j 4)
+            :fioak
+            (= j 3)
+            :fooak
+          (= j 2)
+            :toak
+          (= j 1)
+            :op
+          :else
+            (if (and (jokered? cvs) (= k \J)) :op :hc))
+
+      0 nil)))
 
 (def hand-values [ :hc :op :tp :toak :fh :fooak :fioak ])
 
-(defn frequentistest-value [s]
-  (let [values (set card-values)
-        s' (filter values s)]
-    (reduce-kv
-      #(if (and (some? %1) (> %3 (:v %1))) {:k %2 :v %3} %1)
-      {:v -1}
-      (frequencies s'))))
+(def hand-type (memoize (fn [cvs x]
+  (let [hand (:hand x)
+        ty (.indexOf hand-values (hand-type' cvs hand))]
+    ;(println "Hand" hand "is type" (get hand-values ty))
+    ty
+    ))))
 
-(defn noak [n s]
-  (let [{k :k, v :v} (frequentistest-value s)]
-    (if (>= v n) k nil)))
-
-(defn noak? [n s] (some? (noak n s)))
-
-(defn hand-type' [s]
-  (let [{k :k, v :v} (frequentistest-value s)
-        s' (filter #(not= k %) s)]
-    (case v
-      5 :fioak
-      4 :fooak
-      3 (if (= :op (hand-type' s')) :fh :toak)
-      2 (if (= :op (hand-type' s')) :tp :op)
-      1 :hc
-      -1 nil)))
-
-(defn hand-type [x]
-  (let [hand (:hand x)]
-    (.indexOf hand-values (hand-type' hand))))
-
-(defn p1' [s]
+(defn solve [cvs s]
   (->> s
       str/split-lines
       (map #(str/split % #" " 2))
       (map #(hash-map :hand (first %) :bid (second %)))
-      (sort-by card-strength)
-      (sort-by hand-type)
+      (sort-by (partial card-strength cvs))
+      (sort-by (partial hand-type cvs))
       (map :bid)
       (map parse-long)
       (map-indexed #(* (inc %1) %2))
       (apply +)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn p1' [s] (solve basic-card-values s))
 
-(defn p2' [s] (println "unimplemented"))
+(defn p2' [s] (solve jokered-card-values s))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
